2cf06c3e5b60a298af5b0ac73c83892cdb4a466e
shaxa1207 <shakhbozpulatov@gmail.com>
2021-08-06 17:55:52 +0300

    error table

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,23 +2,53 @@
   <div class="home d-flex">
     <ch-aside />
     <div class="main-part">
-      <search-bar />
+      <search-bar :card="cardItem"/>
+      <ch-table />
     </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import ChAside from "../components/ch-aside.vue";
 import SearchBar from "../components/searchBar.vue";
+import ChTable from "../components/ch-table.vue";
+
 
 export default {
   name: "Home",
   components: {
     ChAside,
     SearchBar,
+    ChTable,
   },
+  data(){
+    return{
+      cardItem: [
+        {
+          img: require("../assets/images/svg4.svg"),
+          title: "Appointments",
+          titleNumber: "213"
+        },
+        {
+          img: require("../assets/images/svg3.svg"),
+          title: "New Patients",
+          titleNumber: "104"
+        },
+        {
+          img: require("../assets/images/svg2.svg"),
+          title: "Operations",
+          titleNumber: "24"
+        },
+        {
+          img: require("../assets/images/svg1.svg"),
+          title: "Hospital Earnings",
+          titleNumber: "$ 12,174"
+        },
+      ]
+    }
+  }
 };
+
 </script>
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap");
